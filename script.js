8b42b691d627b33013afd5ec2d36cf5d474a7b21
Vaishali Yadav <yvaishali011@gmail.com>
2023-07-01 19:08:25 +0530

    made the html ,css and js pages incorporating necessay details

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example JavaScript code
+document.addEventListener('DOMContentLoaded', function() {
+    // Code to execute after the HTML document has been fully loaded
+    console.log('Hello, world!');
+})
